d0e73deb9df10bf2fe4b2b2ee1dc103b67362f52
chinudev <chinumontu@yahoo.com>
2024-09-16 10:58:07 -0700

    This test code didn't work because OpenAI api has changed. Temp commit and then run migrate script

---
+++ langchain/hello_model.py
@@ -0,0 +1,19 @@
+import os 
+import openai
+
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+llm_model = "gpt-3.5-turbo"
+
+def get_completion(prompt, model=llm_model):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, 
+    )
+    return response.choices[0].message["content"]
+
+get_completion("what is one plus one ?")  # Test the function
